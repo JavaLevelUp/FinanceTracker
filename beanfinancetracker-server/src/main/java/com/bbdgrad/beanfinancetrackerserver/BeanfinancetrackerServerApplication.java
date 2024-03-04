4017b4133630c377ffb359237efccfbde6f58b2e
JesseNaidoo <156646712+JesseNaidoo@users.noreply.github.com>
2024-03-04 20:13:35 +0200

    Generated server project

---
+++ beanfinancetracker-server/src/main/java/com/bbdgrad/beanfinancetrackerserver/BeanfinancetrackerServerApplication.java
@@ -0,0 +1,13 @@
+package com.bbdgrad.beanfinancetrackerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanfinancetrackerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeanfinancetrackerServerApplication.class, args);
+	}
+
+}
