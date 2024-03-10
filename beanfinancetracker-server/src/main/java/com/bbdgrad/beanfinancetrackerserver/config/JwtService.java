6d0b2651fd0f32a32050d4c0654b5250753cf640
JesseNaidoo <156646712+JesseNaidoo@users.noreply.github.com>
2024-03-10 12:30:12 +0200

    Changed JWT token expire date

--- beanfinancetracker-server/src/main/java/com/bbdgrad/beanfinancetrackerserver/config/JwtService.java
+++ beanfinancetracker-server/src/main/java/com/bbdgrad/beanfinancetrackerserver/config/JwtService.java
@@ -9,6 +9,7 @@ import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -59,7 +60,7 @@ public class JwtService {
                 .setClaims(extraClaims)
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 *24))
+                .setExpiration(new Date(System.currentTimeMillis() + TimeUnit.HOURS.toMillis(24)))
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
