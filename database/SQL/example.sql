e4565bdf66b1cfd422f5f77d2ed66d98752e1b05
Shaelin Naidoo <Shaelin.Naidoo@standardbank.co.za>
2024-03-12 11:28:33 +0200

    temporarily allow db migration from all branches

--- database/SQL/example.sql
+++ database/SQL/example.sql
@@ -1,6 +1,6 @@
 --changeset nshaelin:example:createTable:example
 --comment: Create example table
 CREATE TABLE "example" (
-  "example_column_id" serial PRIMARY KEY NOT NULL,
+  "example_primary_key_column_id" serial PRIMARY KEY NOT NULL
 );
 --rollback DROP TABLE "example";
