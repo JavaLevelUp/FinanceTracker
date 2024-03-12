08ef7cd8d5b640d0a5bfab9a3f620f70d8cca1c7
JesseNaidoo <156646712+JesseNaidoo@users.noreply.github.com>
2024-03-12 20:48:33 +0200

    Removed JWT

--- beanfinancetracker-server/src/main/java/com/bbdgrad/beanfinancetrackerserver/controller/HelloWorld.java
+++ beanfinancetracker-server/src/main/java/com/bbdgrad/beanfinancetrackerserver/controller/HelloWorld.java
@@ -1,8 +1,6 @@
 package com.bbdgrad.beanfinancetrackerserver.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,8 +16,4 @@ public class HelloWorld {
         return ResponseEntity.ok("Hello from our API");
     }
 
-    @GetMapping("/user")
-    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
-        return Collections.singletonMap("name", principal.getAttribute("name"));
-    }
 }
--- beanfinancetracker-server/src/main/java/com/bbdgrad/beanfinancetrackerserver/model/User.java
+++ beanfinancetracker-server/src/main/java/com/bbdgrad/beanfinancetrackerserver/model/User.java
@@ -5,9 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import java.sql.Timestamp;
 import java.util.Collection;
@@ -20,7 +17,7 @@ import java.util.List;
 @AllArgsConstructor
 @Entity
 @Table(name = "_user")
-public class User implements UserDetails {
+public class User {
     @Id
     @GeneratedValue
     private Integer id;
@@ -30,38 +27,5 @@ public class User implements UserDetails {
     private Timestamp created_at;
     @Enumerated
     private Role role;
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority(role.name()));
-    }
 
-    @Override
-    public String getPassword() {
-        return password;
-    }
-
-    @Override
-    public String getUsername() {
-        return email;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
 }
